f6ce39f8c3f99c2e878bee9de0e24cc8fac9a228
Paweł <pawel.janik.1983@gmail.com>
2021-06-10 22:33:21 +0200

    code not passing attempt, only simple checks

---
+++ cubic_numbers.js
@@ -0,0 +1,46 @@
+const string = "0 1531532 -827&()";
+const string2 = "aqdf&0#1xyz!22[153(777.777"
+const string3 = "QK29a45[&erui9026315"
+
+function isSumOfCubes(s) {
+    const strings = s.split(' ')
+    let numbers = []
+    let cubic = []
+
+
+    strings.map(string => {
+        numbers.push(string.match( /(\d){1,3}/g ) )
+    }) 
+    numbers = [].concat(...numbers)
+
+  
+    let test = numbers.map(number => [...number].reduce((acc, cVal) => {
+        return acc + power(cVal)
+    }, 0))
+
+    console.log(test + ' ' + numbers)
+  
+    numbers.map((number,i) => {
+        if (number == test[i]) {
+            return cubic.push(Number.parseInt(number) )
+        } 
+    })
+  
+    if(cubic.length === 0) {
+      return `Unlucky`
+    } else {
+      let sum = cubic.reduce((acc,cVal) => acc + cVal)
+      cubic.push(sum)
+      console.log(cubic.join(' ') + ` Lucky` + `  Type:` + typeof cubic)
+      console.log(`Length of cubic is: ` + cubic.length)
+      return cubic.join(' ') + ` Lucky`
+    }
+}
+
+function power(element) {
+    return Math.pow(element, 3)
+}
+
+isSumOfCubes(string)
+isSumOfCubes(string2)
+isSumOfCubes(string3)
