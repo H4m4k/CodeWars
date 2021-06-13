f13c88894cae761a6758994b2ae7566894a56158
Paweł <pawel.janik.1983@gmail.com>
2021-06-13 12:38:11 +0200

    numbers array is properly filtered from any string, cubic array is properly filtered from numbers array

--- cubic_numbers.js
+++ cubic_numbers.js
@@ -3,13 +3,27 @@ const string2 = "aqdf&0#1xyz!22[153(777.777"
 const string3 = "QK29a45[&erui9026315"
 
 function isSumOfCubes(s) {
-    const regexp = /(\d){1,3}/g   // filter string with regular exx
-    let numbers = [...s.match(regexp)]
+    const regexp = /(\d){1,3}/g   
+    const numbers = [...s.match(regexp)] // filter string with regular expression and spread the matches on an array
+    let sums = []
     let cubic = []
+ 
+    sums = numbers.map(number => [...number].reduce((acc,cval) => { // spread each element of numbers array to calculate the sum of power 3 
+        return acc + power(cval)
+    },0))
 
+    sums = sums.map( sum => sum.toString()) // convert the integers back to strings to use the strict compare , as two arrays elements will never be equal
+   
+    cubic = numbers.filter((number,i) => numbers.includes(sums[i]))
+   
+    
     console.log(numbers)
+    console.log(`
+                    ${sums}
+                                
+                    ${cubic}`)
 
-  
+    console.log(isCubic(numbers, sums[1]))
     // let test = numbers.map(number => [...number].reduce((acc, cVal) => {
     //     return acc + power(cVal)
     // }, 0))
@@ -31,12 +45,17 @@ function isSumOfCubes(s) {
 //       console.log(`Length of cubic is: ` + cubic.length)
 //       return cubic.join(' ') + ` Lucky`
 //     }
-// }
+}
 
-// function power(element) {
-//     return Math.pow(element, 3)
+function power(number) {
+    return Math.pow(number, 3)
+}
+
+function isCubic(array , sum) {
+    return array.includes(sum)
 }
 
 isSumOfCubes(string);
 isSumOfCubes(string2);
 isSumOfCubes(string3);
+
