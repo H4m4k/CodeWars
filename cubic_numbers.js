6d16a3020bc05b97d61792f2c27cae099729846d
Paweł <pawel.janik.1983@gmail.com>
2021-06-13 20:06:11 +0200

    attempt to replace 000 or 00 with one 0 using and ordinary function

--- cubic_numbers.js
+++ cubic_numbers.js
@@ -1,20 +1,25 @@
-const string = "0 1531532 -827&()";
-const string2 = "aqdf&0#1xyz!22[153(777.777"
-const string3 = "QK29a45[&erui9026315"
+const string = "371407298a --- dreary, ###100.153 I thought, 9926315strong -127&() 1";
+const string2 = "153000153407000407";
+const string3 = "QK29a45[&erui9026315";
+const regzero = /(0){2,3}/g
 
 function isSumOfCubes(s) {
     const regexp = /(\d){1,3}/g   
     const numbers = [...s.match(regexp)] // filter string with regular expression and spread the matches on an array
     let sums = []
     let cubic = []
- 
+    trimZeroes(numbers)
+    console.log(numbers)
+    
     sums = numbers.map(number => [...number].reduce((acc,cval) => { // spread each element of numbers array to calculate the sum of power 3 
         return acc + power(cval)
     },0))
 
     sums = sums.map( sum => sum.toString()) // convert the integers back to strings to use the strict compare , as two arrays elements will never be equal
    
-    cubic = numbers.filter((number,i) => numbers.includes(sums[i]))
+    cubic = numbers.filter((number,i) => {
+        return numbers.includes(sums[i])
+    })
    
     if (cubic.length === 0) {
         return console.log('Unlucky')
@@ -33,14 +38,20 @@ function isSumOfCubes(s) {
 }
 
 function power(number) {
-    return Math.pow(number, 3)
+    return Math.pow(Number.parseInt(number), 3)
 }
 
 function sumCubic(array) {
     return array.reduce((acc,cval) => acc+Number.parseInt(cval),0)
 }
 
+function trimZeroes(array) {
+    if (array.includes(regzero)) {
+        console.log(`Numbers zawiera zera`)
+        return array = array.splice(array.indexOf('regzero'),1,'0')
+    }
+}
+
 isSumOfCubes(string);
 isSumOfCubes(string2);
 isSumOfCubes(string3);
-
