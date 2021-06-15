1384b5f402d74b4f044a9349124bc27970585208
Paweł <pawel.janik.1983@gmail.com>
2021-06-15 23:36:10 +0200

    all integers are displayed properly as requested in challenge, testing

--- Create Phone Number.js
+++ Create Phone Number.js
@@ -1,10 +1,15 @@
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+createPhoneNumber(testArray);
+
 function createPhoneNumber(numbers){
     if(typeof numbers !== 'object') {
       return 'Input has to be an array of integers'
     } else if (numbers.length > 10 || numbers.length < 10 ) {
       return 'Input accepts 10 integers only'
     }
-    let integers = `(${numbers.join('').substring(0,3)})`
+    let integers = `(${numbers.join('').substring(0,3)}) ${numbers.join('').substring(3,6)}-${numbers.join('').substring(6)}`
+     console.log(integers)
     return integers;
   }
   
