6ce211becf842f68adf66fc082755391378c15f0
Paweł <pawel.janik.1983@gmail.com>
2021-06-21 10:56:48 +0200

    Easy kata to train High Order functions

---
+++ Coding Meetup 1.js
@@ -0,0 +1,26 @@
+//Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe
+
+// filter the array received for the conditions given and return with length
+
+const list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ];
+
+const list2 = [
+    { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+    { firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+  ];
+
+
+  function countDevelopers(list) {
+    let count = list.filter(item => item.continent === 'Europe')
+    return count.filter ( language => language.language === 'JavaScript').length
+  }
+
+  countDevelopers(list1)
+  countDevelopers(list2)
+
+  
