27573695af3478d1365193d3ef07e0aad30a0a68
Paweł <pawel.janik.1983@gmail.com>
2021-06-23 07:52:09 +0200

    Simple check with every function, and yet I had to use web resources to expand my knowledge how every can be used than just simple integer operations

---
+++ Coding Meetup 6.js
@@ -0,0 +1,33 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return either:
+
+//     true if all developers in the list code in the same language; or
+//     false otherwise.
+
+// PJ - Array.every checking current element, against array[index-1]  - based on w3schools resource
+
+const list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+  ];
+
+const list2 = [
+    { firstName: 'Mariami', lastName: 'G.', country: 'Georgia', continent: 'Europe', age: 29, language: 'Python' },
+    { firstName: 'Mia', lastName: 'H.', country: 'Germany', continent: 'Europe', age: 39, language: 'Ruby' },
+    { firstName: 'Maria', lastName: 'I.', country: 'Greece', continent: 'Europe', age: 32, language: 'C' },
+  ];
+
+
+  function isSameLanguage(list) {
+    return console.log(list.every((dev,index,array)=> {
+        if( index === 0) {
+            return true;
+        } else {
+            return (dev.language === array[index-1].language)
+        }
+    }))
+}
+
+  isSameLanguage(list2)
